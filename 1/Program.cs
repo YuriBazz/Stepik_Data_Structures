3a0364ae0fe3525f68102daefed853aea12ec0c9
Yuri Kovach <gim.ekb12@gmail.com>
2024-09-25 14:18:31 +0500

    Доделать вторую задачу

--- 1/Program.cs
+++ 1/Program.cs
@@ -7,7 +7,7 @@ public class Program
     {
         var input = Console.ReadLine();
 
-        var stack = new Stack<char>();
+        var stack = new MyStack<(char, int)>();
 
         var opened = new HashSet<char> { '(', '[','{'};
         var closed = new HashSet<char> { ')', ']', '}' };
@@ -19,16 +19,39 @@ public class Program
         for (int i = 0; i < input.Length; i++)
         {
             if (opened.Contains(input[i]))
-                stack.Push(input[i]);
+                stack.Push((input[i],i));
             if (closed.Contains(input[i]))
             {
-                if (stack.Count == 0 || brackets[input[i]] != stack.Pop())
+                if (stack.Count == 0 || brackets[input[i]] != stack.Pop().Item1)
                 { Console.WriteLine(i + 1); return; }
             }
         }
         if (stack.Count == 0)
             Console.WriteLine("Success");
         else
-            Console.WriteLine(input.Length);
+            Console.WriteLine(stack.Pop().Item2 + 1);
     }
-} 
+}
+
+public class MyStack<T>
+{
+    private readonly Stack<T> Stack;
+    public T First { get; private set; }
+    public T Pop()
+    {
+        if (Stack.Count == 0) throw new InvalidProgramException();
+        return Stack.Pop();
+    }
+
+    public void Push(T input)
+    {
+        Stack.Push(input);
+    }
+
+    public int Count { get { return Stack.Count; } }
+
+    public MyStack()
+    {
+        this.Stack = new Stack<T>();
+    }
+}
