5bf2923b6109f4e41c94cba8f0d85dd03729e294
Yuri Kovach <gim.ekb12@gmail.com>
2024-09-28 10:21:13 +0500

    Доделать третью задачу

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var input = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+        var processor = new Processor(input[0]);
+        for(int i = 0; i < input[1]; ++i)
+        {
+
+        }
+    }
+}
+
+public class Processor
+{
+    public int BufferSize { get; private set; }
+
+    public Processor(int bufferSize)
+    {
+        BufferSize = bufferSize;
+    }
+
+    private Queue<(int, int)> buffer = new Queue<(int, int)> ();
+
+    public void AddToBuffer((int,int) input)
+    {
+        if (buffer.Count == BufferSize) return;
+        buffer.Enqueue(input);
+    }
+}
