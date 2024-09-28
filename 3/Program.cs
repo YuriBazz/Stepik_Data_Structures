5dc5cd9b74e398401e7ee9a4d83158f12bb40afe
George Maslov <gim.ekb12@gmail.com>
2024-09-28 12:33:30 +0500

    Для себя третью решил

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,12 +1,31 @@
-﻿public class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
 {
     public static void Main(string[] args)
     {
         var input = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+        var currentTime = 0;
         var processor = new Processor(input[0]);
+
         for(int i = 0; i < input[1]; ++i)
         {
+            var package = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            while (processor.BufferCount != 0 && processor.Peek() <= package[0])
+            {
+                processor.RemoveFromBuffer();
+            }
 
+            currentTime = Math.Max(currentTime, package[0]);
+            currentTime += package[1];
+            if (!processor.AddToBuffer(currentTime))
+                Console.WriteLine(-1);
+            else
+                Console.WriteLine(currentTime - package[1]);
+
+            
         }
     }
 }
@@ -20,11 +39,27 @@ public class Processor
         BufferSize = bufferSize;
     }
 
-    private Queue<(int, int)> buffer = new Queue<(int, int)> ();
+    private Queue<int> buffer = new Queue<int> ();
 
-    public void AddToBuffer((int,int) input)
+    public bool AddToBuffer(int input)
     {
-        if (buffer.Count == BufferSize) return;
+        if (buffer.Count == BufferSize) return false;
         buffer.Enqueue(input);
+        return true;
     }
+
+    public int BufferCount {  get { return buffer.Count; } }
+
+    public int RemoveFromBuffer()
+    {
+        if (BufferCount == 0) throw new InvalidOperationException();
+        return buffer.Dequeue();
+    }
+
+    public int Peek()
+    {
+        if (buffer.Count == 0) throw new InvalidOperationException();
+        return buffer.Peek();
+    }
+   
 }
