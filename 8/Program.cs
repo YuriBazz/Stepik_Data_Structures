f759eef1500f78022255571e6a14609ad499f4dd
George Maslov <gim.ekb12@gmail.com>
2024-10-06 16:16:44 +0500

    Мне похуй

--- 8/Program.cs
+++ 8/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 
 public class Program
 {
@@ -9,52 +8,64 @@ public class Program
     {
         var token = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
         int n = token[0], m = token[1];
-        var tables = new (int,int)[n];
+        var tables = new Table[n];
         token = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
-        for(int i = 0; i < n; ++i)
+        for (int i = 0; i < n; ++i)
         {
-            tables[i] = (i + 1, token[i]);
+            tables[i] = new Table(i + 1, token[i]);
         }
-        var currentMax = tables.Max(x => x.Item2);
-        for(int i = 0; i < m ; ++i)
+        var currentMax = tables.Max(x => x.Size);
+        for (int i = 0; i < m; ++i)
         {
             token = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
             var destination = tables[token[0] - 1];
             var sourse = tables[token[1] - 1];
-            
+            while (destination.IsRefer)
+                destination = destination.Reference;
+            while (sourse.IsRefer)
+                sourse = sourse.Reference;
+            if (!sourse.Equals(destination))
+            {
+                sourse.RewriteTo(destination);
+                currentMax = Math.Max(currentMax, destination.Size);
+            }
             Console.WriteLine(currentMax);
         }
     }
-
-    
-
-    private static void Union(this int[] array,int i,int j)
-    {
-        int x = array.Find(i);
-    }
 }
 
-public class ArrayExtention
+public class Table
 {
-    public static int Find(this int[] array, int i)
+    public int Number { get; private set; }
+    public bool IsRefer { get { return Reference != null; } }
+    public Table? Reference { get; private set; }
+    public int Size { get; private set; }
+
+    public void RewriteTo(Table table)
     {
-        if (i != array[i])
-            array[i] = Find(array, array[i]);
-        return array[i];
+        if (IsRefer)
+            Reference.RewriteTo(table);
+        table.Size += Size;
+        Size = 0;
+        Reference = table;
+    }
+
+    public Table(int number, int size)
+    {
+        Number = number;
+        Size = size;
+        Reference = null;
     }
 
     public override bool Equals(object? obj)
     {
-        return base.Equals(obj);
+        if (obj == null) return false;
+        if (!(obj is Table)) return false;
+        var table = obj as Table;
+        return this.Number == table.Number;
     }
-
     public override int GetHashCode()
     {
-        return base.GetHashCode();
-    }
-
-    public override string? ToString()
-    {
-        return base.ToString();
+        return Number.GetHashCode();
     }
 }
